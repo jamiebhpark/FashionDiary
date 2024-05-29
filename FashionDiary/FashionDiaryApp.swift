62d07bbfb2ead43a6eba46e59a98020115add385
JAMIEPARK90 <jamiebhpark@gmail.com>
2024-05-29 12:54:26 +0900

    Initial Commit

---
+++ FashionDiary/FashionDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FashionDiaryApp.swift
+//  FashionDiary
+//
+//  Created by JONGHUN PARK on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FashionDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
